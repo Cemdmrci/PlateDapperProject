9fae4d903e4fc709e06839a8fcbc2101007dd564
cemdemircii <demircicem1907@gmail.com>
2025-02-14 19:05:31 +0300

    Dashboard eklendi

---
+++ PlateDapperProject/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlateDapperProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
